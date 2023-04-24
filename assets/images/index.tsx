5f14805bb870e6668acc4aac8714d7db3fb66ae4
junhp7 <junhp1994@gmail.com>
2023-04-24 16:46:10 +0900

    이미지 수정

--- assets/images/index.tsx
+++ assets/images/index.tsx
@@ -1,6 +1,2 @@
 export { default as MainBg } from './main_bg.svg';
 export { default as AboutBg } from './about_bg.svg';
-export { default as ProjectImg01 } from './project_img01.jpg';
-export { default as ProjectImg02 } from './project_img02.png';
-export { default as ProjectImg03 } from './project_img03.jpg';
-export { default as ProjectImg04 } from './project_img04.jpg';
